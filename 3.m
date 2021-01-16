70b7d409c3e73f088fa0fb64aadb829b9b580a68
Erdem <57351007+ErdemGns@users.noreply.github.com>
2021-01-17 00:47:32 +0300

    Update 3.m

--- 3.m
+++ 3.m
@@ -1,28 +1,34 @@
 clear; close all; clc;
 
-% 180 çizgili küre çiz
+% 180 boylam-enlem
 [x,y,z] = sphere(180);
 % Yarıçap ayarla
 radius = 6373000;
 x = x * radius;
 y = y * radius;
 z = z * radius;
-% Konum ayarla
+% Dengeleme için
 offset = 6373000;
-surf(x+offset,y+offset,z+offset) 
+surf(x+offset,y+offset,z+offset)
 % Etiket yazdır
 xlabel('X', 'FontSize', 20);
 ylabel('Y', 'FontSize', 20);
 zlabel('Z', 'FontSize', 20);
 axis equal;
 
-% hold on
-% [X,Y,Z] = sphere;
-% r = 100;
-% surf(X,Y,Z)
-% axis equal
-% X2 = X * r;
-% Y2 = Y * r;
-% Z2 = Z * r;
-% surf(X2,Y2,Z2)
+% Uydu için
+% [x,y,z] = sphere(180);
+% % Yarıçap ayarla
+% radius = 100;
+% x = x * radius;
+% y = y * radius;
+% z = z * radius;
+% 
+% offset1 = 637300034535;
+% offset2 = 637300034535;
+% offset3 = 6373000345345;
+% surf(x+offset1,y+offset2,z+offset3)
+% axis equal;
+
+
 
