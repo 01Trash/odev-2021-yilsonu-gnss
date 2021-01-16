5b2f55a9e9be8c498f71c43d924d56ffdf29fa9e
Erdem <57351007+ErdemGns@users.noreply.github.com>
2021-01-16 19:18:51 +0300

    Update 2.m

--- 2.m
+++ 2.m
@@ -1,19 +1,19 @@
 clear; close all; clc;
 
-
 %%% IGS BROADCAST EPHEMERIS FILE verileri
 
-t sinyalin alıcıda gözlendigi sistem anı
-t − τsr sinyalin uydu anteninden çıktıgı sistem anı
-Parametre Tanım
-δtr(t) = tr − t alıcı saat hatası
-δts(t − τsr) = ts − t uydu saat hatası
-dτr(t) alıcı donanım gecikmesi
-dτs(t − τsr) uydu donanım gecikmesi
-Φsr(t) Ta¸sıyıcı dalga faz farkı (tamsayı bilinmeyeni dahil)
-Nsr Ba¸slangıç tamsayı faz bilinmeyeni
-dIsr(t) sinyal yolu iyonosfer gecikmesi
-dTsr(t) sinyal yolu troposfer gecikmesi
-dmsP,r(t), dmsΦ,r(t) sinyal yansıma (multipath) gecikmesi (kod ve faz)
-esP,r(t), esΦ,r(t) rasgele gözlem hataları (kod ve faz)
+% t sinyalin alıcıda gözlendiği sistem anı
+% t - Tsr sinyalin uydu anteninden çıktıgı sistem anı
+
+% Parametre              =>       Tanım
+% δtr(t) = tr - t        =>  alıcı saat hatası
+% δts(t - Tsr) = ts - t  =>  uydu saat hatası
+% dTr(t)                 =>  alıcı donanım gecikmesi
+% dTs(t - Tsr)           =>  uydu donanım gecikmesi
+% Φsr(t)                 =>  Taşıyıcı dalga faz farkı (tamsayı bilinmeyeni dahil)
+% Nsr                    =>  Başlangıç tamsayı faz bilinmeyeni
+% dIsr(t)                =>  sinyal yolu iyonosfer gecikmesi
+% dTsr(t)                =>  sinyal yolu troposfer gecikmesi
+% dmsP,r(t), dmsΦ,r(t)   =>  sinyal yansıma (multipath) gecikmesi (kod ve faz)
+% esP,r(t), esΦ,r(t)     =>  rasgele gözlem hataları (kod ve faz)
 
